bcdb8a12dd5c17b3d1f9a9ead753c9dd32c582ab
Kaupo Juhkam <kaupo@credy.eu>
2016-04-22 12:31:28 +0300

    implement ApiClient

--- src/Zf2Interspire/ApiClient.php
+++ src/Zf2Interspire/ApiClient.php
@@ -2,14 +2,83 @@
 
 namespace Zf2Interspire;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
+
 class ApiClient extends Configurable {
 
     public $apiToken;
     public $apiUser;
     public $host;
     public $endPoint;
+    public $format = 'html';
+    public $confirmed = 'yes';
 
-    public function __call() {
-        # do magic
+    private $client;
+    private $xml;
+
+    public function getClient() {
+        if(!isset($this->_lient)) {
+            $this->client = new Client();
+        }
+        return $this->client;
+    }
+
+    public function getXml() {
+        if(!isset($this->xml)) {
+            $this->xml = new Xml();
+        }
+        return $this->xml;
+    }
+
+    public function addSubcscriber($email, $listId, array $customFields = [], $format = 'html', $confirmed = 'yes') {
+        $details = [
+            'format'       => $format,
+            'confirmed'    => $confirmed,
+            'emailaddress' => $email,
+            'mailinglist'  => $listId,
+            'customfields' => $customFields,
+        ];
+
+        $result = $this->getXml()->toArray($this->sendRequest('AddSubscriberToList', $details));
+        if(!count($customFields)) {
+            return;
+        }
+        if (strpos($result['errormessage'], 'already exists in the given list') !== false) {
+            $this->updateCustomFields($email, $listId, $customFields);
+        }
+    }
+
+    public function updateCustomFields($email, $listId, array $customFields = []) {
+        $details = [
+            'emailaddress' => $email,
+            'mailinglist'  => $listId,
+            'customfields' => $customFields,
+        ];
+
+        $this->sendRequest('updatesubscriber', $details);
+    }
+
+    private function sendRequest($requestMethod, array $details) {
+
+        $payload = [
+            'username'      => $this->apiUser,
+            'usertoken'     => $this->apiToken,
+            'requesttype'   => 'subscribers',
+            'requestmethod' => $requestMethod,
+            'details'       => $details,
+        ];
+
+        try {
+            return $this->getClient()->post(
+                $this->endPoint,
+                [
+                    'headers' => ['Host' => $this->host],
+                    'body'    => $this->getXml()->fromArray($payload)
+                ]
+            )->getBody()->getContents();
+        } catch (BadResponseException $e) {
+            return $e->getResponse()->getBody()->getContents();
+        }
     }
 }
---
+++ src/Zf2Interspire/Xml.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Zf2Interspire;
+
+use XMLWriter;
+
+class Xml extends Configurable {
+
+    public $startElement = 'xmlrequest';
+
+    public function toArray($string) {
+        return (new \Zend\Config\Reader\Xml())->fromString($string);
+    }
+
+    public function fromArray(array $array) {
+        $writer = new XMLWriter();
+        $writer->openMemory();
+        $writer->setIndent(true);
+        $writer->setIndentString(str_repeat(' ', 4));
+
+        $writer->startDocument('1.0', 'UTF-8');
+        $writer->startElement($this->startElement);
+
+        foreach ($array as $sectionName => $data) {
+            if (!is_array($data)) {
+                $writer->writeElement($sectionName, (string) $data);
+            } else {
+                $this->addBranch($sectionName, $data, $writer);
+            }
+        }
+
+        $writer->endElement();
+        $writer->endDocument();
+
+        return $writer->outputMemory();
+    }
+
+
+    /**
+     * Add a branch to an XML object recursively.
+     *
+     * @param  string    $branchName
+     * @param  array     $config
+     * @param  XMLWriter $writer
+     *
+     * @throws \Exception
+     */
+    protected function addBranch($branchName, array $config, XMLWriter $writer)
+    {
+        $branchType = null;
+
+        foreach ($config as $key => $value) {
+            if ($branchType === null) {
+                if (is_numeric($key)) {
+                    $branchType = 'numeric';
+                } else {
+                    $writer->startElement($branchName);
+                    $branchType = 'string';
+                }
+            } elseif ($branchType !== (is_numeric($key) ? 'numeric' : 'string')) {
+                throw new \Exception('Mixing of string and numeric keys is not allowed');
+            }
+
+            if ($branchType === 'numeric') {
+                if (is_array($value)) {
+                    $this->addBranch($branchName, $value, $writer);
+                } else {
+                    $writer->writeElement($branchName, (string) $value);
+                }
+            } else {
+                if (is_array($value)) {
+                    $this->addBranch($key, $value, $writer);
+                } else {
+                    $writer->writeElement($key, (string) $value);
+                }
+            }
+        }
+
+        if ($branchType === 'string') {
+            $writer->endElement();
+        }
+    }
+}
